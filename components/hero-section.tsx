d685df1b4ab5a227226a1a0b8d8f1fe658bc1d22
Nandeeswar Badugu <nandeeswarbadugu@Nandeeswars-MacBook-Air.local>
2025-02-18 19:42:20 -0600

    landing page

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -1,14 +1,40 @@
+"use client"
 import Link from 'next/link'
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { Button } from './ui/button'
 import Image from 'next/image'
 
 const HeroSection = () => {
+
+    const imageRef = useRef(null);
+
+    useEffect(() => {
+        const imageElement = imageRef.current;
+
+        const handleScroll = () => {
+            const scrollPosition = window.scrollY;
+            const scrollThreshold = 100;
+
+            if (scrollPosition > scrollThreshold) {
+                //@ts-ignore
+                imageElement.classList.add("scrolled");
+            } else {
+                //@ts-ignore
+                imageElement.classList.remove("scrolled");
+            }
+        }
+
+        window.addEventListener("scroll",handleScroll);
+        return () => window.removeEventListener("scroll",handleScroll);
+
+
+    },[]);
+
     return (
         <section className='w-full pt-36 md:pt-48 pb-10'>
             <div className='space-y-6'>
                 <div className='text-center'>
-                    <h1 className='text-5xl pb-5'>
+                    <h1 className='text-5xl font-bold md:text-6xl lg:text-7xl xl:text-8xl gradient-title animate-gradient'>
                         Your AI Career Coach for
                         <br />
                         Professional Success
@@ -31,10 +57,10 @@ const HeroSection = () => {
                         </Button>
                     </Link>
                 </div>
-                <div>
-                    <div>
+                <div className='hero-image-wrapper mt-5 md:mt-0'>
+                    <div ref = {imageRef} className='hero-image'>
                         <Image src='/banner.jpeg'
-                         width={1280}
+                         width={1400}
                          height={720}
                          alt="Dashboard Preview"
                          className="rounded-lg shadow-2xl border mx-auto"
